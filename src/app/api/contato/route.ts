228efb975faaf80e8f14697827c5e84a5eacad20
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2025-01-28 11:12:32 -0300

    Upload

---
+++ src/app/api/contato/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server"
+
+export async function GET() {
+    const vCardData = `BEGIN:VCARD
+VERSION:3.0
+FN:SALVIO PARTNER LTDA
+TITLE:José Henrique Salvio
+ROLE:Your Business Partner
+TEL;TYPE=CELL:+55 31 99851-8916
+TEL;TYPE=WORK,VOICE:+55 28 3344-1022
+EMAIL:josehenrique@salviopartner.com
+URL;type=WORK:www.salviopartner.com
+NOTE:Instagram: @salviopartner  Your Business PARTNER
+END:VCARD`
+
+    return new NextResponse(vCardData, {
+        headers: {
+            'Content-Type': 'text/vcard',
+            'Content-Disposition': 'attachment; filename="contact.vcf"',
+        }
+    })
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+export default function Home() {
+
+  const saveVcard = async () => {
+    try {
+      const response = await fetch('/api/contato')
+      const blob = await response.blob()
+
+      //Cria um link temporario para forçar o Download.
+      const url = window.URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = url
+      a.download = 'contact.vcf'
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }catch(error){
+      console.error('Erro ao salvar contato: ', error)
+      alert('Erro ao salvar contato!')
+    }
+   
+  }
+
+  return (
+    <button onClick={saveVcard}>
+      Salvar Contato
+    </button>
+  )
+}
