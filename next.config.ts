f5c1c46016822856c686d58ffae3ad5609167f66
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2025-01-28 11:41:47 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/contato",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/vcard",
+          },
+          {
+            key: "Content-Disposition",
+            value: 'attachment; filename="contato-salviopartner.vcf"',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
